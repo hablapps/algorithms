1714202b6c590afbc4b5f0dd0138bb5c4e8c212d
tortolavivo23 <tortolavivo23@gmail.com>
2021-10-13 18:11:55 +0200

    Comments

--- src/main/scala/CYK.scala
+++ src/main/scala/CYK.scala
@@ -1,13 +1,15 @@
 object CYK {
-  //La función recibe inp, que es la cadena que queremos comprobar que esté en la cadena
-  //simbols que es una array con las reglas generadoras, está formada por dos duplas, el primer término es el generador
-  //El segundo término es lo generado, que lo generado puede ser un elemento(Left()), o dos reglas generadoras (Right())
-  //Devuelve true si el elemento se puede generar con esa gramática y false en caso contrario
+  //The function receives inp, the string that we want to chek it it´s int the grammar
+  //simbols is an array with the generation rules, its made with tuples, the first element its the generator
+  //The second element it´s the generate, it can be an elemnent(Left()) or two generators(Right())
+  //The function gives back true if inp is in the grammar, or false in the other case
+  //its necesary that the grammar is a Chosky normal form, and the start rules are (0, _)
   def CYK (inp: String, simbols: Array[(Int, Either[Char, (Int,Int)])]): Boolean={
     var unit: List[(Int, Char)] = List()
     var production : List[(Int, (Int, Int))] = List()
-    var set: Set[Int] = Set()
-    for(s<- simbols)
+    var set: Set[Int] = Set() //set is use to count how many different generators there are
+    //We split the generation rules in two, unit the rules that produce elements and production the rules that produce two rules
+    for(s<- simbols) {
       s match {
         case (a, Left(b)) =>
           unit = (a,b) :: unit
@@ -18,22 +20,26 @@ object CYK {
           set += a
 
       }
+    }
+    //We create an auxiliar function with all the parameters
     def function(inp: Array[Char], unit: List[(Int, Char)], production: List[(Int, (Int, Int))],  r: Int): Boolean={
       val n = inp.length
       val P = Array.ofDim[Boolean](n, n, r)
-      //Comprobamos con que reglas generadoras se pueden generar los carácteres de la cadena
+      //We check the generator rules that can generate the characters in inp
       for (i <- 1 to n)
         for (a<- unit)
-          if(a._2 == inp(i-1)) P(i-1)(0)(a._1)=true
-      for (i<- 2 to n)
-        for(j<- 1 to n-i+1)
-          for(k<- 1 to i-1)
+          if(a._2 == inp(i-1)) P(i-1)(0)(a._1)=true //when an element can be generated with a rule we mark it in the array
+      //We check all the substrings of inp if it can be generated with the rules
+      for (i<- 2 to n)//Length of substring
+        for(j<- 1 to n-i+1)//Start of substring
+          for(k<- 1 to i-1)//Position of substring
             for(s <- production)
               s match {
+                //We check if the rule produce what we have checked before
                 case (a, (b, c)) if(P(j-1)(k-1)(b) & P(j+k-1)(i-k-1)(c))=>P(j-1)(i-1)(a)= true
                 case _ =>
               }
-      P(0)(n-1)(0)
+      P(0)(n-1)(0) //We return if inp can be generated with starter simbols.
     }
     function(inp.toCharArray, unit, production, set.size)
   }
